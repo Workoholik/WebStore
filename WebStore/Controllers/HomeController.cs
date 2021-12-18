99760cdb1eec5cf5adb1054317be1d90ea74d159
Александр Ватропин <webdevelopment.it@gmail.com>
2021-12-18 16:05:53 +0300

    Добавление первого контроллера

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        { 
+
+            return Content("Мой первый контроллер");
+        }
+    }
+}
--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,7 +1,30 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+var configurations = builder.Configuration;
+var services = builder.Services;
+
+
+configurations.AddJsonFile("appsettings.Custom.json", true, true);
+configurations.AddCommandLine(args);
+
+services.AddControllersWithViews();
+
+
 var app = builder.Build();
 
-// Подтянем данные из конфига
-app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+if (app.Environment.IsDevelopment()) 
+{
+    app.UseDeveloperExceptionPage();    
+}
+
+
+app.UseRouting();
+
+// app.MapGet("/", () => configurations["CustomGreetings"]);
+app.MapGet("/throw", () => {
+    throw new ApplicationException(configurations["CustomException"]);
+});
+
+app.MapDefaultControllerRoute();
 
 app.Run();
