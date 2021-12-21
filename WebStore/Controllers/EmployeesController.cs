2dba658d9b7021e2ca88ce6a0ec21c00b7fd3eaa
Александр Ватропин <webdevelopment.it@gmail.com>
2021-12-21 17:22:11 +0300

    Контролер сотрудники Представления сотрудники Доработка частичного представления меню

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+        private static readonly Dictionary<string, Department> department = new Dictionary<string, Department>()
+        {
+            {
+                "IT", new Department { Code = "it", Title = "IT" }
+            },
+            {
+                "bookkeping", new Department { Code = "bookkeping", Title = "Бухгалтерия" }
+            },
+        };
+
+        private static readonly List<Employee> __Employee = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 25, Department = department["IT"] },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 32, Department = department["IT"] },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 46, Department = department["bookkeping"] }
+        };
+
+        public IActionResult Index(int id)
+        {
+            if (id > 0)
+            {
+                return View("Employee", __Employee[id - 1]);
+            }
+            else
+            {
+                return View(__Employee);
+            }
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -17,40 +17,11 @@ namespace WebStore.Controllers
             new Menu {Title = "Департаменты", Code = "department" },
         };
 
-        private static readonly Dictionary<string, Department> department = new Dictionary<string, Department>()
-        {
-            {
-                "IT", new Department { Code = "it", Title = "IT" }
-            },
-            {
-                "bookkeping", new Department { Code = "bookkeping", Title = "Бухгалтерия" }
-            },
-        };
-
-        private static readonly List<Employee> __Employee = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 25, Department = department["IT"] },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 32, Department = department["IT"] },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 46, Department = department["bookkeping"] }
-        };
-
         public IActionResult Index()
-        { 
-
+        {  
             return View(__Menu);
         }
-
-        public IActionResult Employees(int id)
-        { 
-            if (id > 0)
-            {
-                return View("Employee", __Employee[id-1]);
-            } 
-            else
-            {
-                return View("Employees", __Employee);
-            }
-        }
+         
 
         public string ConfiguredAction(string id, string Value1)
         { 
