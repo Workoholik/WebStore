1fd9efd0a9a101a82de5c71414b23419a2fcf03a
Александр Ватропин <webdevelopment.it@gmail.com>
2021-12-22 16:45:57 +0300

    Разнес все макеты по представлениям

--- WebStore/Controllers/CatalogController.cs
+++ WebStore/Controllers/CatalogController.cs
@@ -8,15 +8,26 @@ namespace WebStore.Controllers
     {
         private static readonly List<Product> __Products = new()
         {
-            new Product { Id = 1, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product12.jpg" },
-            new Product { Id = 2, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product11.jpg" },
-            new Product { Id = 3, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product10.jpg" },
-            new Product { Id = 4, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product9.jpg" },
-            new Product { Id = 5, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product8.jpg" },
-            new Product { Id = 6, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product12.jpg" },
-            new Product { Id = 7, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product11.jpg" },
-            new Product { Id = 8, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product10.jpg" }, 
-            new Product { Id = 9, Name = "Easy Polo Black Edition", Price = 56, Image= "/images/shop/product9.jpg" }, 
+            new Product { Id = 1, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product12.jpg" },
+            new Product { Id = 2, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product11.jpg" },
+            new Product { Id = 3, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product10.jpg" },
+            new Product { Id = 4, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product9.jpg" },
+            new Product { Id = 5, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product8.jpg" },
+            new Product { Id = 6, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product12.jpg" },
+            new Product { Id = 7, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product11.jpg" },
+            new Product { Id = 8, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product10.jpg" },
+            new Product { Id = 9, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product9.jpg" },
+            new Product { Id = 10, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product8.jpg" },
+            new Product { Id = 11, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product12.jpg" },
+            new Product { Id = 12, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product11.jpg" },
+            new Product { Id = 13, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product10.jpg" },
+            new Product { Id = 14, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product9.jpg" },
+            new Product { Id = 15, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product8.jpg" },
+            new Product { Id = 16, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product12.jpg" },
+            new Product { Id = 17, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product11.jpg" },
+            new Product { Id = 18, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product10.jpg" },
+            new Product { Id = 19, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product9.jpg" },
+            new Product { Id = 120, Name = "Easy Polo Black Edition", Price = 56, Image = "/images/shop/product8.jpg" },
         };
          
         public IActionResult Index()
@@ -25,6 +36,6 @@ namespace WebStore.Controllers
             return View(result);
         }
 
-        public IActionResult Details() => View();
+        public IActionResult Details() => View(__Products);
     }
 }
